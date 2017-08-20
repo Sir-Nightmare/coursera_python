0d81f873d5ff7db73265d6c8689ad2cfb07dc1fa
Sir Nightmare <ivantochilkin@gmail.com>
2017-08-20 20:31:44 +0300

    add two tasks

---
+++ solution.py
@@ -0,0 +1,14 @@
+class FileReader:
+    def __init__(self, path):
+        self.path=path
+
+    def read(self):
+        try:
+            with open(self.path, 'r') as f:
+                return f.read()
+        except IOError:
+            return ''
+
+if __name__ == '__main__':
+    reader = FileReader("example.txt")
+    print(reader.read())
