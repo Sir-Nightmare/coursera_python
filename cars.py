f9e92f357b3db8af9a1706373c924d4f71688541
Sir Nightmare <ivantochilkin@gmail.com>
2017-08-20 20:51:13 +0300

    make inits

--- cars.py
+++ cars.py
@@ -1,24 +1,38 @@
 import csv
+import os
 
 
 class CarBase:
     def __init__(self, brand, photo_file_name, carrying):
-        pass
+        self.brand = brand
+        self.photo_file_name = photo_file_name
+        self.carrying = carrying
+
+    def get_photo_file_ext(self):
+        return os.path.splitext(self.photo_file_name)[1]
 
 
 class Car(CarBase):
     def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
-        pass
+        super().__init__(brand, photo_file_name, carrying)
+        self.car_type = 'car'
+        self.passenger_seats_count = passenger_seats_count
 
 
 class Truck(CarBase):
     def __init__(self, brand, photo_file_name, carrying, body_whl):
-        pass
+        super().__init__(brand, photo_file_name, carrying)
+        self.car_type = 'truck'
+        self.body_whl = body_whl
 
+    def get_body_volume(self):
+        pass
 
 class SpecMachine(CarBase):
     def __init__(self, brand, photo_file_name, carrying, extra):
-        pass
+        super().__init__(brand, photo_file_name, carrying)
+        self.car_type = 'spec_machine'
+        self.extra = extra
 
 
 def get_car_list(csv_filename):
@@ -30,3 +44,7 @@ def get_car_list(csv_filename):
         for row in reader:
             print(row)
     return car_list
+
+
+if __name__ == '__main__':
+    print(os.path.splitext('olo.jpg')[1])
