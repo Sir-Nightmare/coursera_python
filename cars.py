b045fcd02039ba1e11da68efbc3d5fb423c15fc6
sirnightmare <sir-nightmare@yandex.ru>
2017-08-21 19:19:08 +0300

    making funcs

--- cars.py
+++ cars.py
@@ -13,26 +13,56 @@ class CarBase:
 
 
 class Car(CarBase):
-    def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
+    def __init__(self, brand, passenger_seats_count, photo_file_name, carrying):
         super().__init__(brand, photo_file_name, carrying)
         self.car_type = 'car'
         self.passenger_seats_count = passenger_seats_count
 
 
 class Truck(CarBase):
-    def __init__(self, brand, photo_file_name, carrying, body_whl):
+    def __init__(self, brand, photo_file_name, body_whl, carrying):
         super().__init__(brand, photo_file_name, carrying)
         self.car_type = 'truck'
-        self.body_whl = body_whl
+        self.body_width = 0
+        self.body_height = 0
+        self.body_length = 0
+        if body_whl:
+            whl = body_whl.split('x')
+            self.body_length = float(whl[0])
+            self.body_width = float(whl[1])
+            self.body_height = float(whl[2])
 
     def get_body_volume(self):
-        pass
+        return self.body_width * self.body_height * self.body_length
+
 
 class SpecMachine(CarBase):
     def __init__(self, brand, photo_file_name, carrying, extra):
         super().__init__(brand, photo_file_name, carrying)
         self.car_type = 'spec_machine'
         self.extra = extra
+
+
+def make_car(row):
+    brand = row[1]
+    passenger_seats_count = row[2]
+    photo_file_name = row[3]
+    carrying  = row[5]
+
+
+def make_truck(row):
+    brand = row[1]
+    passenger_seats_count = row[2]
+    photo_file_name = row[3]
+    carrying  = row[5]
+
+
+def make_truck(row):
+    brand = row[1]
+    passenger_seats_count = row[2]
+    photo_file_name = row[3]
+    carrying = row[5]
+    extra  = row[6]
 
 
 def get_car_list(csv_filename):
@@ -42,9 +72,12 @@ def get_car_list(csv_filename):
         reader = csv.reader(csv_fd, delimiter=';')
         next(reader)
         for row in reader:
-            print(row)
-    return car_list
+            try:
+                pass
+            except:
+                pass
+        return car_list
 
 
 if __name__ == '__main__':
-    print(os.path.splitext('olo.jpg')[1])
+    print(get_car_list('cars.csv'))
